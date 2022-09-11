1505fbe67c6b4bccb941843289b3ab92199358fa
Potato <poltoradnevone@gmail.com>
2022-09-11 07:55:03 +0400

    fixin bugs

--- src/controllers/seed.js
+++ src/controllers/seed.js
@@ -1,4 +1,19 @@
-CREATE TABLE users (
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createTable = void 0;
+const db_1 = require("../db/db");
+const createTable = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    yield (0, db_1.query)(`
+	CREATE TABLE users (
 	id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
 	username VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL UNIQUE,
@@ -8,6 +23,7 @@ CREATE TABLE users (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+   
 
 CREATE TABLE reviews (
 	id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
@@ -19,7 +35,7 @@ CREATE TABLE reviews (
 
 CREATE TABLE products (
 	id UUID DEFAULT gen_random_uuid () PRIMARY KEY,
-	title VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
 	slug VARCHAR(255) NOT NULL,
 	image VARCHAR(255) DEFAULT '',
 	category VARCHAR(255) NOT NULL,
@@ -69,7 +85,8 @@ CREATE TABLE orders (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 	FOREIGN KEY (payment_id) REFERENCES payments(id),
 	FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
+
 
 CREATE table order_product (
 	order_id UUID NOT NULL,
@@ -84,6 +101,13 @@ CREATE table product_image (
 	FOREIGN KEY (product_id) REFERENCES products(id)
 );
 
+INSERT INTO products (name, slug, image, category,
+  description, brand, price, count_in_stock, rating) VALUES ('Printed cotton poplin bowling shirt', 'cotton-bowling-shirt', 'https://res.cloudinary.com/dxf7urmsh/image/upload/v1662573185/694125_ZAJSS_7371_004_100_0000_Light-Printed-cotton-poplin-bowling-shirt_wkjsln.jpg', 'Shirts', 'Pursuing the concept of escapism and faraway destination, Gucci Love Parade presents a series of pieces inspired by travel to warm places. Prints recalling typical souvenir designs, create playful yet elegant ready-to-wear pieces.', 'Gucci', 999, 1, 4 );
 
-INSERT INTO products (title, slug, image, category,
-  description, brand, price, count_in_stock, rating) VALUES ('Nice Shoes', 'shoes-gucci', 'https://imgur.com/QmWPxXA.png', 'Shoes', 'Derby shoes with five-eye lacing. Rounded nose.', 'Gucci', 499, 100, 2 );
+
+  INSERT INTO products (name, slug, image, category,
+  description, brand, price, count_in_stock, rating) VALUES ('Wool sweater with Gucci patch', 'pullover-gucci', 'https://res.cloudinary.com/dxf7urmsh/image/upload/v1662572729/694786_XKCD5_4804_002_100_0000_Light--Gucci_fjoia2.jpg', 'Sweaters', 'A blue crewneck sweater crafted from knitted wool. Refreshed sweater styles instill a contemporary feel into traditional silhouettes, the knit Gucci patch gives a subtle nod the House heritage.', 'Gucci', 499, 10, 5 );
+    `, []);
+    res.send('Tables created!');
+});
+exports.createTable = createTable;
